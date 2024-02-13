b9d5ace54f877771a2c12bfe98e6711a85fb5588
Giovanni <g.dalessandro@almaviva.it>
2024-02-13 15:59:50 +0100

    merging

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,7 @@ import { AccessService } from '../services/access.service';
 import { AccessJson } from '../models/accessJson';
 import { LoadingService } from '../services/loading.service';
 import { FirebaseService } from '../services/firebase.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
